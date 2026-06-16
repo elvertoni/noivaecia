06b011938a00026e6f68ffc8ac8e5270454b2fc7
elvertoni <elvertoni@gmail.com>
2026-06-16 18:26:41 -0300

    chore: remove throwaway debug script

--- _tmp_check.py
+++
@@ -1,4 +0,0 @@
-from customers.models import Customer
-for val in ["BTES","ITCA","ANDIRA","ITANBARACA","S BTES","BTS","ABATIA"]:
-    n = Customer.objects.filter(city=val).count()
-    print(f"{n:>6}  {val!r}")
